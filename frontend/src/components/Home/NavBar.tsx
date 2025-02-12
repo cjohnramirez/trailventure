e7545c8f2585059fded23b2aaf2ad3d7de019122
cjohnramirez <jcramirez340@gmail.com>
2025-02-12 10:03:18 +0800

    Navbar and Homepage improvements

--- frontend/src/components/Home/NavBar.tsx
+++ frontend/src/components/Home/NavBar.tsx
@@ -1,32 +1,139 @@
-import { Menu, Search, User } from "lucide-react";
+import { useState } from "react";
+import {
+  BedDouble,
+  Calendar as CalendarIcon,
+  Menu,
+  Minus,
+  Plus,
+  Search,
+  User,
+  Users,
+} from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import {
   DropdownMenu,
+  DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/dropdown-menu";
+import { Popover, PopoverTrigger } from "@radix-ui/react-popover";
+import { PopoverContent } from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 
 function NavBar() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  const roomOptions: string[] = ["rooms", "adults", "children"];
+
+  const [roomOptionState, setRoomOptionState] = useState({
+    rooms: 1,
+    adults: 1,
+    children: 1,
+  });
+
   return (
-    <div className="flex-col border-[1px] bg-white p-4 shadow-md dark:bg-[#09090b]">
+    <div className="flex-col border-[1px] bg-white px-4 pt-4 shadow-md dark:bg-[#09090b]">
       <div className="flex items-center justify-between">
-        <Button variant="outline">Add</Button>
-        <p className="font-extrabold">TRAILVENTURE</p>
-        <div className="flex border-[1px] rounded-md">
+        <Button variant="outline">
+          <Plus />
+          <p>Add Item</p>
+        </Button>
+        <p className="text-xl font-extrabold">TRAILVENTURE</p>
+        <div className="flex rounded-md border-[1px]">
           <DropdownMenu>
-            <DropdownMenuTrigger className="px-2">
-              <Menu />
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                <Menu />
+              </Button>
             </DropdownMenuTrigger>
+            <DropdownMenuContent className="mr-9 w-56">
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Messages</DropdownMenuItem>
+                <DropdownMenuItem>Notifications</DropdownMenuItem>
+                <DropdownMenuItem>Trips</DropdownMenuItem>
+                <DropdownMenuItem>Wishlist</DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>About Us</DropdownMenuItem>
+                <DropdownMenuItem>Log out</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
           </DropdownMenu>
           <Button variant="ghost">
             <User />
           </Button>
         </div>
       </div>
-      <div className="flex items-center justify-center pt-4">
-        <Input className="h-[50px] md:max-w-[500px] rounded-full" />
+      <div className="my-4 flex w-full items-center justify-center">
+        <div className="flex items-center gap-4 rounded-full border-[1px] pl-4 sm:w-full md:w-[600px]">
+          <Search className="min-w-[15px]" />
+          <Input
+            className="h-[50px] rounded-full border-none px-4"
+            placeholder="Search"
+          ></Input>
+          <div className="flex gap-4 pr-4">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" className="rounded-full">
+                  <div className="flex items-center gap-2">
+                    <p>{date?.toLocaleDateString()}</p>
+                    <CalendarIcon />
+                  </div>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Calendar mode="single" selected={date} onSelect={setDate} />
+              </PopoverContent>
+            </Popover>
+            <Button variant="outline" className="w-30 rounded-full">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <div className="flex items-center gap-2">
+                    {roomOptionState.rooms}
+                    <BedDouble />
+                    {roomOptionState.adults + roomOptionState.children}
+                    <Users />
+                  </div>
+                </PopoverTrigger>
+                <PopoverContent className="mr-4 mt-2 pb-[2px]">
+                  <div>
+                    {roomOptions.map((option, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center justify-between gap-2 pb-4"
+                      >
+                        <p className="text-sm">{option.substring(0, 1).toLocaleUpperCase() + option.substring(1)}</p>
+                        <div className="flex gap-2 items-center justify-end">
+                          <Button variant="outline" className="rounded-full w-8" onClick={() => {
+                            setRoomOptionState((prevState) => ({
+                              ...prevState,
+                              [option]: prevState[option as keyof typeof prevState] + 1
+                            }));
+                          }}>
+                            <Plus />
+                          </Button>
+                          {roomOptionState[option as keyof typeof roomOptionState]}
+                          <Button variant="outline" className="rounded-full w-8" onClick={() => {
+                            setRoomOptionState((prevState) => ({
+                              ...prevState,
+                              [option]: prevState[option as keyof typeof prevState] - 1
+                            }));
+                          }}>
+                            <Minus />
+                          </Button>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,33 +1,11 @@
 import { useEffect, useState } from "react";
 import api from "@/api";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Check } from "lucide-react";
-import { ModeToggle } from "@/components/mode-toggle";
 import NavBar from "@/components/Home/NavBar";
-
-interface UserRegisterInformation {
-  email: string;
-  first_name: string;
-  last_name: string;
-  username: string;
-}
-
-interface UserProfile {
-  date_of_birth: string;
-  phone_number: string;
-  user: UserRegisterInformation;
-}
+import Banaue from "../assets/Banaue.jpg";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 function Home() {
-  const [userData, setUserData] = useState<UserProfile[]>([]);
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
     getUserData();
@@ -43,45 +21,29 @@ function Home() {
       .catch((err) => alert(err));
   };
 
-  const UserDataArray: Array<keyof UserRegisterInformation> = [
-    "email",
-    "first_name",
-    "last_name",
-    "username",
-  ];
-
   return (
     <div>
       <div className="sticky top-0">
         <NavBar />
       </div>
-      <div className="flex h-screen w-full items-center justify-center gap-2">
-        <Card>
-          <CardHeader>
-            <CardTitle>User Information</CardTitle>
-            <CardDescription>
-              Here are some information about the user
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <ModeToggle />
-            {userData.length > 0 &&
-              UserDataArray.map((userDataText, index) => (
-                <div
-                  key={index}
-                  className="border-black-800 my-2 rounded-lg border p-3"
-                >
-                  <p className="text-sm font-semibold">
-                    {userDataText.replace("_", " ").toUpperCase()}
-                  </p>
-                  <p className="text-sm">{userData[0]["user"][userDataText]}</p>
-                </div>
-              ))}
+      <div className="p-4">
+        <Card className="pt-6 md:flex">
+          <CardContent className="md:w-1/2 lg:w-2/3">
+            <img
+              src={Banaue}
+              className="w-full rounded-lg object-cover md:h-full md:max-h-[500px]"
+            />
           </CardContent>
-          <CardFooter>
-            <Button className="w-full">
-              <Check /> Noiceee
-            </Button>
+          <CardFooter className="flex-col gap-y-6 text-left md:w-1/2 md:justify-center md:py-20 lg:w-1/3">
+            <p className="w-full text-[50px] leading-none">EXPLORE THE UNEXPLORED.</p>
+            <p className="text-sm">
+              We invite you to venture beyond the familiar and discover new
+              possibilities. It challenges you to step outside your comfort
+              zone, embracing the unknown with curiosity and courage. Whether
+              it's uncharted places, fresh ideas, or hidden potentials, there's
+              always something waiting to be uncovered. Dare to go beyond
+              boundaries and redefine what's possible.
+            </p>
           </CardFooter>
         </Card>
       </div>
