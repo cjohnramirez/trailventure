317865a99639ab28be3032b0deffdd20b8b0ee0d
cjohnramirez <jcramirez340@gmail.com>
2025-02-26 11:08:12 +0800

    Minor backend changes

--- backend/apps/packages/models.py
+++ backend/apps/packages/models.py
@@ -5,22 +5,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class PackageImage(models.Model):
-    image = models.ImageField(upload_to="Package_images/")
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Package Image for Package: {self.Package.name}"
-
 
 class Package(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
     host = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="Package", null=True
-    )
-    package_image = models.ForeignKey(
-        PackageImage, on_delete=models.CASCADE, related_name="Package", null=True
     )
     destination = models.ForeignKey(
         Destination, on_delete=models.CASCADE, related_name="Package", null=True
@@ -30,22 +20,27 @@ class Package(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
-    def clean(self):
-        if self.end_date <= self.start_date:
-            raise ValidationError("End date must be after the start date.")
-
     def __str__(self):
         return f"Package: {self.name}"
+
+
+class PackageImage(models.Model):
+    image = models.ImageField(upload_to="package_images/")
+    created = models.DateTimeField(auto_now_add=True)
+    package = models.ForeignKey(
+        Package, related_name="package_image", on_delete=models.CASCADE, null=True
+    )
+
+    def __str__(self):
+        return f"Package Image for package {self.package.name}"
 
 
 class PackageRoutePoint(models.Model):
     class Meta:
         verbose_name_plural = "Package route points"
-    
+
     package = models.ForeignKey(
-        Package,
-        on_delete=models.CASCADE,
-        related_name="itineraries"
+        Package, on_delete=models.CASCADE, related_name="itineraries"
     )
     title = models.CharField(max_length=255)
     point_number = models.PositiveIntegerField(null=True, blank=True)
@@ -57,9 +52,11 @@ class PackageRoutePoint(models.Model):
     modified = models.DateTimeField(auto_now=True)
 
     count = 0
-    def __str__(self):
-        return f"Package {self.package.name} Route Point {self.point_number}: {self.title}"
 
+    def __str__(self):
+        return (
+            f"Package {self.package.name} Route Point {self.point_number}: {self.title}"
+        )
 
 
 class PackageAmenity(models.Model):
@@ -83,11 +80,10 @@ class PackageType(models.Model):
     package = models.ForeignKey(
         Package, on_delete=models.CASCADE, related_name="Package", null=True
     )
-    package_amenity = models.ForeignKey(
-        PackageAmenity, on_delete=models.CASCADE, related_name="Package", null=True
+    package_amenity = models.ManyToManyField(
+        PackageAmenity, related_name="Package", null=True
     )
     description = models.TextField()
-    icon_name = models.CharField(max_length=255, unique=False, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
