40d97992eee9bee033447eb667d82f9c8dcbe09e
cjohnramirez <jcramirez340@gmail.com>
2025-03-08 13:15:58 +0800

    Added Footer and new HomePage component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer pl-20 py-8 mt-8 bg-[#f4f4f5] dark:bg-[#09090b]">
+      <div className="container flex justify-center">
+        <p>&copy; 2023 Trailventure. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,9 @@ import NavBar from "@/components/NavBar/NavBar";
 import DiscoverSection from "@/components/Home/DiscoverSection";
 import BackgroundImage from "../assets/Home/HomePage.webp";
 import { useMediaQuery } from "react-responsive";
+import Footer from "@/components/Footer/Footer";
+import { Calendar, SearchIcon, Shield, User } from "lucide-react";
+import personHiking from "../assets/Home/personHiking.jpeg";
 
 function Home() {
   const [forNavBar, setforNavBar] = useState(true);
@@ -32,7 +35,7 @@ function Home() {
   return (
     <div>
       <div className="sticky top-0 z-20 bg-[#ffffff] px-8 py-4 dark:bg-[#09090b]">
-        <NavBar isNavBar={!forNavBar} isHomePage={true}/>
+        <NavBar isNavBar={!forNavBar} isHomePage={true} />
       </div>
       <div className="flex h-screen w-full select-none flex-col px-8 pb-[100px]">
         <div className="relative flex h-full w-full flex-col items-center justify-center">
@@ -51,8 +54,48 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="mt-[-100px] px-8" id="section1">
+      <div className="mt-[-90px] px-8" id="section1">
         <DiscoverSection />
+      </div>
+      <div className="px-8" id="section1">
+        <div className="flex flex-col justify-center">
+          <div className="leading-none">
+            <p className="relative text-center text-[40px] font-extrabold leading-none md:text-[40px] lg:text-[70px]">
+              CHOOSE ME? AHAHAHAHA
+            </p>
+            <p className="pb-12 pt-4 text-center font-semibold md:pb-20">
+              Your No.1 Tour Package Booking Platform
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto grid max-w-[1200px] grid-cols-3 grid-rows-2 gap-4">
+          <div className="flex flex-col justify-center col-span-1 rounded-2xl border-[1px] p-8 shadow-lg">
+            <Calendar size={64} strokeWidth={1} className="mb-4" />
+            <p className="text-2xl font-semibold">Hassle-Free Booking</p>
+            <p>A fast, easy, and worry-free tour package booking process.</p>
+          </div>
+          <div className="col-span-1 row-span-2 rounded-2xl border-[1px] shadow-lg">
+            <img src={personHiking} className="object-cover h-full rounded-2xl"></img>
+          </div>
+          <div className="flex flex-col justify-center col-span- rounded-2xl border-[1px] p-8 shadow-lg">
+            <SearchIcon size={64} strokeWidth={1} className="mb-4" />
+            <p className="text-2xl font-semibold">Advanced Search</p>
+            <p>Find the best tour packages with our advanced search options.</p>
+          </div>
+          <div className="flex flex-col justify-center col-span-1 rounded-2xl border-[1px] p-8 shadow-lg">
+            <User size={64} strokeWidth={1} className="mb-4" />
+            <p className="text-2xl font-semibold">Designed for You</p>
+            <p>Personalized recommendations to suit your preferences.</p>
+          </div>
+          <div className="flex flex-col justify-center col-span-1 rounded-2xl border-[1px] p-8 shadow-lg">
+            <Shield size={64} strokeWidth={1} className="mb-4" />
+            <p className="text-2xl font-semibold">Secure and Trusted Bookings</p>
+            <p>Book with confidence knowing your bookings are secure.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
