e33eb11605b8e5c489dd9c5ccd5252c8520950c3
cjohnramirez <jcramirez340@gmail.com>
2025-03-08 15:02:06 +0800

    Critical fix on search page

--- frontend/src/lib/SearchPage/dataFormatter.ts
+++ frontend/src/lib/SearchPage/dataFormatter.ts
@@ -1,5 +1,5 @@
 export const formatDate = (dateString : string) => {
   const [month, day, year] = dateString.split("-");
-  const date = new Date(`${year}-${month}-${day}`);
+  const date = new Date(`${month}-${day}-${year}`);
   return new Intl.DateTimeFormat('en-CA').format(date); // 'en-CA' gives 'YYYY-MM-DD' format
 };
